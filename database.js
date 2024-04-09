722245e0bba972898726e712a5cdfc66fc29c197
Chris Fellows <chris.fellows@OKTiK.net>
2024-04-09 19:02:18 +0100

    chore: Code cleanup

--- database.js
+++ database.js
@@ -1,5 +1,16 @@
 import mysql from "mysql"
 
+//console.log("Connecting to database")
+//const db = mysql.createConnection({
+//    host: "localhost",
+//    //port: 3306,
+//    user: "root",
+//    password: "kAn68sq9B218s",    
+//    database: "cfforum"
+//})
+//export default db
+//console.log("Connected to database")
+
 // TODO: Move to config
 console.log("Creating database pool")
 const connectionPool  = mysql.createPool({
